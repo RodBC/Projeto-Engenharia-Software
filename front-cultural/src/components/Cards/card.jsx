0217a8d40446f7e4b6d2437dd9d970294fcd301b
Tanha <acmsp@cin.ufpe.br>
2023-07-10 13:11:07 -0300

    mudanças feitas no card:(botão de curtida, botão de compartilhamento, animação de colorir o botão)

--- front-cultural/src/components/Cards/card.jsx
+++ front-cultural/src/components/Cards/card.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import "./style.css";
-import reactLogo from '../../assets/rec_antigo.jpg'
 import montanha from '../../assets/fotoCool.jpeg'
+import heart from '../../assets/heart.png'
+import share from '../../assets/share.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+
 
 export const Card = () => {
-    const [count, setCount] = useState(0);
 
+    const [curtida, setCurtida] = useState(false);
+    const muda_curtida = ()=>{
+      setCurtida(!curtida)
+    }
+    
     return (
     <>
       <div className="card-space">
@@ -14,14 +23,18 @@ export const Card = () => {
           <div className='imagem'>
             <img src={montanha} alt="" />
           </div>
+
           <div className='card-content'>
             <h2>Iniciativa</h2>
+
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum ipsum, aut impedit nisi facere nesciunt fugiat at ipsam molestiae velit exercitationem porro, nulla quae repellendus quis! Id unde ipsa voluptate?</p>
-            <div className='button'>
+
+            <div className='buttons'>
               <a href="https://www.youtube.com/watch?v=HHOn8u-c2wk&list=LL&index=12&t=593s" id="Texto_Do_Butão">Saiba amais</a>
+              <FontAwesomeIcon icon={faHeart} className="heart_button"  color={curtida? 'red':'grey'} onClick={()=>muda_curtida()}/>
+              <img src={share} id="share_button"/>
             </div>
           </div>
-          
         </div>
       </div>
 
