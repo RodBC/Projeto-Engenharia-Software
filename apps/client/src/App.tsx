8c63b6b20abc9c20151433661e3cb52a5e24e9a3
Tanha <acmsp@cin.ufpe.br>
2023-08-15 18:29:58 -0300

    testando a integração entre os componentes

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,12 +1,26 @@
-// import { Card } from "./components/Cards/card"
+import { Card } from "./components/Cards/card"
 import {Navbar} from "./components/Navbar/navbar.jsx"
-// import "./style.css";
+import './app_style.css'
 export const App = () => {
 
   return (
     <>
       <Navbar/>
-      {/* <Card/> */}
+
+      <div className="title1">
+        <h1>Placeholder for title</h1>
+      </div>
+      
+      <div className="locations_buttons">
+        <h1>Placeholder for locations</h1>
+      </div>
+      
+      <div className="space_for_cards">
+        <Card/>
+        <Card/>
+        <Card/>
+
+      </div>
     </>
   )
 }
