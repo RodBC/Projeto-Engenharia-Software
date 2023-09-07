0236a7e54766c22ab5267f96225ea8b13254734b
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-09-07 19:36:12 -0300

    instalando styled components e estilizando RegisterPage

---
+++ application/apps/client/src/components/RegisterPage/Input/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  outline:  #046ee5;
+  padding: 16px 20px;
+  width: 100%;
+  border-radius: 5px;
+  font-size: 16px;
+  background-color: #f0f2f5;
+  border: none;
+`;
