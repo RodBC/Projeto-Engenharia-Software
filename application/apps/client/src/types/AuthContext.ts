b2b4dbb29d3d5dedef00a1c573eb36fa1dea9e6d
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-09-07 00:16:13 -0300

    adicionando o sign up

--- application/apps/client/src/types/AuthContext.ts
+++ application/apps/client/src/types/AuthContext.ts
@@ -3,6 +3,7 @@ import { User } from "./User";
 export type AuthContextType = {
     user: User | null;
     signIn: (email: string, password: string) => Promise<void>;
+    signUp: (email:string, password: string, name:string) => Promise<void>
     signOut: () => void;
     isAuthenticated: boolean;
   };
